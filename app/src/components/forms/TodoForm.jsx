916e1787ec451ef74be374b25c9fc8e8af7a17cf
AymanSdq <sedqiayman02@gmail.com>
2024-08-23 12:34:39 +0100

    Send From render to main

--- app/src/components/forms/TodoForm.jsx
+++ app/src/components/forms/TodoForm.jsx
@@ -3,24 +3,27 @@ import React, { useState } from 'react'
 
 function TodoForm() {
 
+  // ipcRenderer
+  const ipcRenderer = window.ipcRenderer;
+
   // The initial value of the form
   const [descValue , setDescValue] = useState("");
 
-
   const handleChange = (event) => {
-
     setDescValue(event.target.value)
-
   }
-
 
   // On submit function
   const handleSubmit = (event) => {
-
+    
     event.preventDefault();
+    console.log(descValue);
 
-    console.log(descValue)
+    ipcRenderer.send("submit:todoForm", descValue)
   }
+
+
+
 
   return (
     <div>
