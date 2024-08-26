fafd8f475a27a2c8eeb6b9e79ee4c2ddbbde7008
AymanSdq <sedqiayman02@gmail.com>
2024-08-26 23:23:21 +0100

    Adding api

--- app/src/pages/Work.jsx
+++ app/src/pages/Work.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import TodoForm from '../components/forms/TodoForm';
+import axios from 'axios';
 
 function Work() {
+
+
+
+
+  axios.get("http://localhost:3366/get/todolist")
+    .then(result => {
+      const listTask = result.data.todolists
+      console.log(listTask[0])
+
+    }).catch(error => {
+      console.log(error)
+    })
+
 
 
   const electron = window.electron;
@@ -9,8 +23,13 @@ function Work() {
 
   return (
     <div>
-      
+
       <TodoForm />
+
+      <div className='w-full'>
+
+
+      </div>
 
     </div>
   )
