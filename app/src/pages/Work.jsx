5b5baed9c2e06e76dad6c81f63a0e349c55778e2
AymanSdq <sedqiayman02@gmail.com>
2024-08-27 21:48:49 +0100

    Showing tasks

--- app/src/pages/Work.jsx
+++ app/src/pages/Work.jsx
@@ -1,21 +1,21 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import TodoForm from '../components/forms/TodoForm';
 import axios from 'axios';
 
 function Work() {
 
+  const [listTask, setListTask] = useState([]);
 
 
-
-  axios.get("http://localhost:3366/get/todolist")
-    .then(result => {
-      const listTask = result.data.todolists
-      console.log(listTask[0])
-
-    }).catch(error => {
-      console.log(error)
-    })
-
+  useEffect(() => {
+    axios.get('http://127.0.0.1:3366/get/todolist')
+        .then(result => {
+            setListTask(result.data.todolists);
+        })
+        .catch(error => {
+            console.error("There was an error fetching the todo list!", error);
+        });
+}, []);
 
 
   const electron = window.electron;
@@ -27,7 +27,9 @@ function Work() {
       <TodoForm />
 
       <div className='w-full'>
-
+      {listTask.map((task) => (
+                    <li key={task._id}>{task.description}</li>
+                ))}
 
       </div>
 
